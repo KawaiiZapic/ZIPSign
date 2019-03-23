87b02f847e7d937d436621fa735466c0b1691758
Zapic <45936772+Zapic233@users.noreply.github.com>
2019-03-23 12:32:58 +0800

    Update curl.php
    更换历史上的今天API

--- curl.php
+++ curl.php
@@ -2,7 +2,7 @@
 //获取历史上的今天事件
 function get_history() {
     $hurl = curl_init();
-    curl_setopt($hurl, CURLOPT_URL, "https://apa.me/mz/today.php");
+    curl_setopt($hurl, CURLOPT_URL, "http://www.ipip5.com/today/api.php?type=json");
     curl_setopt($hurl, CURLOPT_HEADER, 0);
     curl_setopt($hurl, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($hurl, CURLOPT_SSL_VERIFYPEER, false);
@@ -10,10 +10,9 @@ function get_history() {
     curl_setopt($hurl, CURLOPT_ENCODING, '');
     curl_setopt($hurl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 3.0.04506; .NET CLR 3.5.21022; .NET CLR 1.0.3705; .NET CLR 1.1.4322)');
     curl_setopt($hurl, CURLOPT_FOLLOWLOCATION, 1);
-    $historyc = curl_exec($hurl);
-    $history = json_decode($historyc, true);
-    $history = $history[mt_rand(1, 15)];
-    $history = str_replace("\n" , "", $history);
+    $data = curl_exec($hurl);
+    $data = json_decode($data,true);
+    $history  = $data['result'][mt_rand(1,count($data['result'])-1)];
     return $history;
 }
 //获取IP对应的地理位置
